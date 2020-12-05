585f0fd37bff7823dbf2219b93fda8b14b61c5f2
imtiaz-m <iemtiaz.m@gmail.com>
2020-12-06 00:01:34 +0600

    retry to add project file

---
+++ app.js
@@ -0,0 +1,19 @@
+const hour = document.getElementById('hour');
+const minutes = document.getElementById('minute');
+const seconds = document.getElementById('second');
+const progress =document.getElementById('progress');
+
+function showCurrentTime(){
+    let date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    hour.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = sec;
+
+    progress.style.width = (sec/60)*100 + '%';
+
+}
+setInterval(showCurrentTime,1000)
